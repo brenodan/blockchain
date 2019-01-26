0e7b584028b8dda02d0a1d4cd5e4d0cae9305a1b
brenodcruz@gmail.com <brenodan@hotmail.com>
2019-01-26 09:32:57 -0500

    modifying Chain class

--- src/main/Chain.java
+++ src/main/Chain.java
@@ -1,9 +1,22 @@
 package main;
 
+import java.util.ArrayList;
+
+import com.google.gson.GsonBuilder;
+
 public class Chain {
 
+	
+	public static ArrayList<Block> blockchain = new ArrayList<Block>();
+	
 	public static void main(String[] args) {
 		
+		blockchain.add(new Block("I am the alpha block", "0"));
+		blockchain.add(new Block("I am the second block", blockchain.get(blockchain.size()-1).hash));
+		blockchain.add(new Block("I am the third block", blockchain.get(blockchain.size()-1).hash));
+		
+		
+		/*
 		Block genesisBlock = new Block("I am the alpha block", "0");
 		System.out.println("Hash for alpha block: " + genesisBlock.hash);
 		
@@ -12,6 +25,40 @@ public class Chain {
 		
 		Block thirdBlock = new Block("I am the third block", secondBlock.hash);
 		System.out.println("Hash for 3rd block: " + thirdBlock.hash);
+		*/
+		
+		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+		System.out.println(blockchainJson);
+		
+	}
+	
+	public static Boolean isChainValid() {
+		
+		Block cBlock;
+		Block pBlock;
+		
+		for (int i = 0; i < blockchain.size(); i++) {
+			
+			cBlock = blockchain.get(i);
+			pBlock = blockchain.get(i - 1);
+			
+			if(!cBlock.hash.equals(cBlock.calculateHash())){
+				
+				System.out.println("Current hashes are not equal");
+				return false;
+			
+			}
+			
+			if(!pBlock.hash.equals(cBlock.previousHash)) {
+				System.out.println("Previous hashes are not equal");
+				return false;
+			}
+			return true;
+			
+		}
+		
+		return true;
+		
 	}
 	
 }
